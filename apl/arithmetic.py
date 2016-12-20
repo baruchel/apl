e3e0f22ed133ad21154c1b602a036dc651be41d7
Thomas Baruchel <baruchel@gmx.com>
2016-12-20 13:23:31 +0100

    Added logarithm.

--- apl/arithmetic.py
+++ apl/arithmetic.py
@@ -9,23 +9,31 @@ add = make_monadic_dyadic_scalar_f(lambda x: x, np.add)
 sub = make_monadic_dyadic_scalar_f(np.negative, np.subtract)
 
 
-def _direction(_right):
+def _direction(right):
     with np.errstate(divide='ignore', invalid='ignore'):
-        return _right/abs(_right)
+        return right/abs(right)
 mul = make_monadic_dyadic_scalar_f(_direction, np.multiply)
 
 
-def _reciprocal(_right):
+def _reciprocal(right):
     with np.errstate(divide='ignore', invalid='ignore'):
-        return 1./_right
+        return 1./right
 div = make_monadic_dyadic_scalar_f(_reciprocal, np.divide)
 
 
-def _residue(_left, _right):
+def _residue(left, right):
     with np.errstate(divide='ignore', invalid='ignore'):
-        return np.mod(_right, _left) + (_left == 0)*_right
+        return np.mod(right, left) + (left == 0)*right
 residue = make_monadic_dyadic_scalar_f(np.abs, _residue)
 
 min = make_monadic_dyadic_scalar_f(np.floor, np.min)
+
+
 max = make_monadic_dyadic_scalar_f(np.ceil, np.max)
+
+
 power = make_monadic_dyadic_scalar_f(np.exp, np.power)
+
+
+log = make_monadic_dyadic_scalar_f(np.log,
+        lambda left, right: np.log(right)/np.log(left))
